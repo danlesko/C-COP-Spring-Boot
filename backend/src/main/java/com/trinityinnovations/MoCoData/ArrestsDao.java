58881da53c5d7cc578d7a99088bbb41769d51541
Joshua Sharo <Josh@Sharo.local>
2017-04-25 13:35:23 -0400

    Correct SQL stament (commented out)

--- backend/src/main/java/com/trinityinnovations/MoCoData/ArrestsDao.java
+++ backend/src/main/java/com/trinityinnovations/MoCoData/ArrestsDao.java
@@ -44,14 +44,17 @@ public class ArrestsDao {
     return arrests;
   }
 }
-//
-//  SELECT
-//  DATE_FORMAT(
-//    MIN(arrest_date),
-//    '%Y/%m/%d'
-//      ) AS tmstamp,
-//      COUNT(*) AS cnt
-//      FROM
-//      arrest
-//      WHERE arrest_date BETWEEN "2017-04-02 12:40:00" AND "2017-04-10 12:40:00"
-//      GROUP BY ROUND(UNIX_TIMESTAMP(arrest_date) / 86400)
+//  SELECT t1.tmstamp, t1.cnt, t2.cnt
+//  from
+//  (SELECT DATE_FORMAT(MIN(arrest_date), '%Y/%m/%d') AS tmstamp,
+//  COUNT(*) AS cnt
+//  FROM arrest
+//  WHERE arrest_date BETWEEN "2017-04-02 12:40:00" AND "2017-04-10 12:40:00"
+//  GROUP BY ROUND(UNIX_TIMESTAMP(arrest_date) / 86400)) t1
+//  LEFT JOIN
+//  (SELECT DATE_FORMAT(MIN(date), '%Y/%m/%d') AS tmstamp,
+//  COUNT(*) AS cnt
+//  FROM crime
+//  WHERE date BETWEEN "2017-04-02 12:40:00" AND "2017-04-10 12:40:00"
+//  GROUP BY ROUND(UNIX_TIMESTAMP(date) / 86400)) t2
+//  ON t1.tmstamp = t2.tmstamp
